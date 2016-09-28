c7a6562639d059de27d0794587ed5634deb7e887
Matt Jones <matt.mattjones@gmail.com>
2016-09-28 16:57:05 -0400

    updated attributes for dna specified verstion

--- cookbooks/php/attributes/php.rb
+++ cookbooks/php/attributes/php.rb
@@ -2,7 +2,23 @@
 #default['php']['minor_version'] = "5.6"
 
 
-default['php']['version'] = "7.0.6"
-default['php']['minor_version'] = "7.0"
+#default['php']['version'] = "7.0.6"
+#default['php']['minor_version'] = "7.0"
 
 default['php']['full_atom'] = "dev-lang/php"
+
+fallback_php_version = case attribute['dna']['engineyard']['environment']['components'].map(&:values).flatten.find(/^php_/).first
+                            when 'php_5'
+                              '5.6.25'
+                            when 'php_7'
+                              '7.0.6'
+                            else
+                              #'7.0.6'
+			      '5.6.25'	
+                            end
+ 
+ default['php']['version'] = node.engineyard.environment.metadata(
+ 'php_version', fallback_php_version
+ )
+
+default['php']['minor_version'] =  default['php']['version'].split(".").first(2).join(".")
