54cfd5fd2c507f3a16b0f721ad622881621b6bc4
Christopher Rigor <crigor@gmail.com>
2016-09-30 14:45:37 +0800

    Dj attributes and README (#26)
    * Add delayed_job4 attributes
    
    Add examples/delayed_job4
    
    * Add ey-custom on delayed_job4 example
    
    * Add custom-delayed_job4 README
    
    * More delayed job readme changes
    
    * Fix path

---
+++ examples/delayed_job4/cookbooks/custom-delayed_job4/attributes/default.rb
@@ -0,0 +1,3 @@
+# default['delayed_job4']['is_dj_instance'] = (node['dna']['instance_role'] == 'util' && node['dna']['name'] == 'delayed_job')
+# default['delayed_job4']['applications'] = %w[todo]
+# default['delayed_job4']['worker_count'] = 4
