cca7720193a61eb2f2dab56c692e1a1bb5ed1dbe
Matt Jones <matt.mattjones@gmail.com>
2016-10-07 19:19:27 -0400

    attributs/php.rb updated for php 5.5, 5.6, and 7.0

--- cookbooks/php/attributes/php.rb
+++ cookbooks/php/attributes/php.rb
@@ -1,14 +1,15 @@
-
 default['php']['full_atom'] = "dev-lang/php"
 
 fallback_php_version = case attribute['dna']['engineyard']['environment']['components'].map(&:values).flatten.find(/^php_/).first
-  when 'php_5'
+  when 'php_55'
+    '5.5.35'
+  when 'php_56'
     '5.6.25'
   when 'php_7'
     '7.0.6'
   else
-   # '7.0.6'
-   '5.6.25'	
+    '7.0.6'
+   #'5.6.25'	
 end
  
 default['php']['version'] = node.engineyard.environment.metadata('php_version', fallback_php_version)
