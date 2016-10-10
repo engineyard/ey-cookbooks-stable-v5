fde3cc8ae3d4e71dd103f5564ceda2a6ed576a46
Allan Espinosa <aespinosa@engineyard.com>
2016-10-11 06:11:53 +0900

    Add metadata.rb information to pass lint checks

---
+++ cookbooks/sidekiq/metadata.rb
@@ -0,0 +1,9 @@
+name 'sidekiq'
+
+version '5.0.0'
+
+maintainer 'Engine Yard, Inc.'
+maintainer_email 'support@engineyard.com'
+
+issues_url 'https://github.com/engineyard/ey-cookbooks-stable-v5/issue'
+source_url 'https://github.com/engineyard/ey-cookbooks-stable-v5'
