881c6a04835d9133f46d836ed910673b1f39a02d
Radamanthus Batnag <radamanthus@gmail.com>
2016-10-12 11:18:01 +0800

    Follow the attributes style used in the sidekiq recipe
    to make modifying individual values easier

--- examples/solr/cookbooks/custom-solr/attributes/solr.rb
+++ examples/solr/cookbooks/custom-solr/attributes/solr.rb
@@ -1,6 +1,14 @@
-default['solr'] = {
-  'java_version'          => '3.0.1',
-  'java_eselect_version'  => 'icedtea-bin-8',
-  'solr_version'          => '6.1.0',
-  'core_name'             => 'default'
-}
+default['solr'].tap do |solr|
+  solr['java_version'] = '3.0.1'
+  solr['java_eselect_version']  = 'icedtea-bin-8'
+  solr['solr_version'] = '6.1.0'
+  solr['core_name'] = 'default'
+
+  # Run Solr on a named util instance
+  # This is the default
+  solr['is_solr_instance'] = (node['dna']['instance_role'] == 'util' && node['dna']['name'] == 'solr')
+
+  # Run Solr on a solo instance
+  # Not recommended for production environments
+  #solr['is_solr_instance'] = (node['dna']['instance_role'] == 'solo')
+end
