82d8bfcb27022057b90635fa59514a6d7d3a8861
rbankston <rbankston@engineyard.com>
2016-10-13 15:24:09 -0400

    More tuning of removing efs volumes

--- cookbooks/efs/recipes/default.rb
+++ cookbooks/efs/recipes/default.rb
@@ -2,6 +2,6 @@ if node['efs']['exists'] == true
   include_recipe "efs::configure"
 end
 
-if node['efs']['exists'] == false
+if node['efs']['exists'] == false &&
   include_recipe "efs:remove"
 end
