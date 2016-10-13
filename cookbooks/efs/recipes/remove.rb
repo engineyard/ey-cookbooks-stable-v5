03dea45fd8059c06a390abe2862c3367e656204e
rbankston <rbankston@engineyard.com>
2016-10-13 17:05:01 -0400

    umount -l

--- cookbooks/efs/recipes/remove.rb
+++ cookbooks/efs/recipes/remove.rb
@@ -1,11 +1,16 @@
 deviceid = File.read('/opt/.efsid').chomp
 mount = node['efs']['mountpoint']
 
+
+execute "umount -l #{mount}" do
+  command "umount -l #{mount}"
+end
+
 mount "#{mount}" do
   device "#{deviceid}:/"
   fstype "nfs4"
   options "rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2"
-  action [:umount, :disable]
+  action :disable
 end
 
 file '/opt/.efsid' do
