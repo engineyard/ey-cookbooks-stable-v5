f93c718b9a1e393cf79a402645b2865c69a7af4f
rbankston <rbankston@engineyard.com>
2016-10-13 15:34:13 -0400

    Move the file check

--- cookbooks/efs/recipes/remove.rb
+++ cookbooks/efs/recipes/remove.rb
@@ -6,7 +6,6 @@ mount "#{mount}" do
   fstype "nfs4"
   options "rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2"
   action [:unmount, :disable]
-  only_if { File.exist?("/opt/.efsid")}
 end
 
 file '/opt/.efsid' do
