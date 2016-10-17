86b4d54384c64f553627d29d59097a7df1559771
rbankston <rbankston@engineyard.com>
2016-10-17 16:10:17 -0400

    Install rack 1.6.4

--- cookbooks/unicorn/recipes/install.rb
+++ cookbooks/unicorn/recipes/install.rb
@@ -2,6 +2,12 @@ ey_cloud_report "unicorn" do
   message "processing unicorn - install"
 end
 
+gem_package 'rack' do
+  version "1.6.4"
+  action :install
+end
+
+
 gem_package 'unicorn' do
   version "4.1.1"
   action :install
