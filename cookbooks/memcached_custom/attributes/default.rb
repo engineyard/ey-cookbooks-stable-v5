518ac028e9131c22e8e64b241c9761cf0f24b336
Radamanthus Batnag <radamanthus@gmail.com>
2016-11-01 16:43:27 +0800

    Change default to install memcached on util instance
    Also removed remnants of solr copy-paste

--- cookbooks/memcached_custom/attributes/default.rb
+++ cookbooks/memcached_custom/attributes/default.rb
@@ -6,9 +6,9 @@
 default['memcached'].tap do |memcached|
 
   # Install memcached on a utility instance named 'memcached'
-  #memcached['install_type'] = 'NAMED_UTILS'
-  #memcached['util_name'] = 'memcached'
+  memcached['install_type'] = 'NAMED_UTILS'
+  memcached['util_name'] = 'memcached'
 
   # Install memcached on all app instances
-  memcached['install_type'] = 'ALL_APP_INSTANCES'
+  #memcached['install_type'] = 'ALL_APP_INSTANCES'
 end
