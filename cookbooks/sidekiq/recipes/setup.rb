48fe31197762c54b04c963ddbd31f21168000e93
dbeckstead <dbeckstead@engineyard.com>
2016-11-28 20:57:13 -0800

    Revert "Allowed configuring workers independently"

--- cookbooks/sidekiq/recipes/setup.rb
+++ cookbooks/sidekiq/recipes/setup.rb
@@ -23,20 +23,21 @@ if node['sidekiq']['is_sidekiq_instance']
       command "monit reload && sleep 10 && monit restart all -g #{app_name}_sidekiq"
       action :nothing
     end
-
+    
     # monit
-    template "/etc/monit.d/sidekiq_#{app_name}.monitrc" do
-      mode 0644
-      source "sidekiq.monitrc.erb"
+    template "/etc/monit.d/sidekiq_#{app_name}.monitrc" do 
+      mode 0644 
+      source "sidekiq.monitrc.erb" 
       backup false
-      variables({
-        :app_name => app_name,
+      variables({ 
+        :app_name => app_name, 
         :workers => node['sidekiq']['workers'],
-        :rails_env => node.dna['environment']['framework_env']
+        :rails_env => node.dna['environment']['framework_env'],
+        :memory_limit => 400 # MB
       })
       notifies :run, "execute[restart-sidekiq-for-#{app_name}]"
     end
-
+    
     # database.yml
     execute "update-database-yml-pg-pool-for-#{app_name}" do
       db_yaml_file = "/data/#{app_name}/shared/config/database.yml"
@@ -47,16 +48,16 @@ if node['sidekiq']['is_sidekiq_instance']
     end
 
     # yml files
-    node['sidekiq']['workers'].each_with_index do |config, count|
+    node['sidekiq']['workers'].times do |count|
       template "/data/#{app_name}/shared/config/sidekiq_#{count}.yml" do
         owner node['owner_name']
         group node['owner_name']
         mode 0644
         source "sidekiq.yml.erb"
         backup false
-        variables(node['sidekiq'].merge(config))
+        variables(node['sidekiq'])
         notifies :run, "execute[restart-sidekiq-for-#{app_name}]"
       end
     end
-  end
+  end 
 end
