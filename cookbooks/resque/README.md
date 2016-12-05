9ffab16af9389905c76bbff868743874456c6b35
Christopher Rigor <crigor@gmail.com>
2016-12-05 18:31:47 +0800

    Rewrite resque recipe for v5
    Create attributes to support customizations

---
+++ cookbooks/resque/README.md
@@ -0,0 +1,8 @@
+# Resque
+
+This recipe is used to run resque on the stable-v5 stack.
+
+The resque recipe is managed by Engine Yard. You should not copy this recipe to your repository but instead copy custom-resque. Please check the [custom-resque readme](../../examples/resque/cookbooks/custom-resque) for the complete instructions.
+
+We accept contributions for changes that can be used by all customers.
+
--- cookbooks/resque/recipes/default.rb
+++ cookbooks/resque/recipes/default.rb
@@ -2,36 +2,28 @@
 # Cookbook Name:: resque
 # Recipe:: default
 #
-if ['solo', 'util'].include?(node[:instance_role])
+if node['resque']['is_resque_instance']
   
   execute "install resque gem" do
     command "gem install resque redis redis-namespace yajl-ruby -r"
     not_if { "gem list | grep resque" }
   end
 
-  case node[:ec2][:instance_type]
-  when 'm1.small' then worker_count = 2
-  when 'c1.medium'then worker_count = 3
-  when 'c1.xlarge' then worker_count = 8
-  else worker_count = 4
-  end
-
-
-  node[:applications].each do |app, data|
-    template "/etc/monit.d/resque_#{app}.monitrc" do
+  node['resque']['applications'].each do |app_name|
+    template "/etc/monit.d/resque_#{app_name}.monitrc" do
       owner 'root' 
       group 'root' 
       mode 0644 
       source "monitrc.conf.erb" 
       variables({ 
-      :num_workers => worker_count,
-      :app_name => app, 
-      :rails_env => node[:environment][:framework_env] 
+      :num_workers => node['resque']['worker_count'],
+      :app_name => app_name, 
+      :rails_env => node['dna']['environment']['framework_env'] 
       }) 
     end
 
-    worker_count.times do |count|
-      template "/data/#{app}/shared/config/resque_#{count}.conf" do
+    node['resque']['worker_count'].times do |count|
+      template "/data/#{app_name}/shared/config/resque_#{count}.conf" do
         owner node[:owner_name]
         group node[:owner_name]
         mode 0644
