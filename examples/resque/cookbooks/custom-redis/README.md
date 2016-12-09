23e6c754c395c21ac4d49a57e2d84b05a533a946
Christopher Rigor <crigor@gmail.com>
2016-12-09 11:09:56 +0800

    Add examples/resque

---
+++ examples/resque/cookbooks/custom-redis/README.md
@@ -0,0 +1,3 @@
+# Custom Redis
+
+This custom-redis recipe is used in the resque example to install redis on the utility instance named "resque" instead of the default "redis".
---
+++ examples/resque/cookbooks/custom-resque/attributes/default.rb
@@ -0,0 +1,3 @@
+# default['resque']['is_resque_instance'] = (node['dna']['instance_role'] == 'util' && node['dna']['name'] == 'resque')
+# default['resque']['applications'] = %w[todo]
+# default['resque']['worker_count'] = 4
