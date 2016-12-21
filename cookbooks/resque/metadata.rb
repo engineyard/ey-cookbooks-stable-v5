9681cc41d8d6bea5a352749347b1fbacff0e9f38
Radamanthus Batnag <radamanthus@gmail.com>
2016-12-21 15:54:23 +0800

    Use version 5.0.0 for resque metadata

--- cookbooks/resque/metadata.rb
+++ cookbooks/resque/metadata.rb
@@ -3,4 +3,4 @@ description 'Configuration & deployment of Resque on Engine Yard'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 maintainer 'Engine Yard'
 maintainer_email 'support@engineyard.com'
-version '0.0.1'
+version '5.0.0'
