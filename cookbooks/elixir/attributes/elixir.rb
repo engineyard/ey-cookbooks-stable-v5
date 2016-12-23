081e98c5636015a4107d2269c4ea34dd046c4a79
rbankston <rbankston@engineyard.com>
2016-12-23 15:13:15 -0500

    Update Elixir Version

--- cookbooks/elixir/attributes/elixir.rb
+++ cookbooks/elixir/attributes/elixir.rb
@@ -2,5 +2,5 @@ default['elixir']['full_atom'] = "dev-lang/elixir"
 default['elixir']['erlang']['full_atom'] = "dev-lang/erlang"
 default['elixir']['config']['file'] = "elixir_app.config"
 
-default['elixir']['version'] = "1.2.5"
+default['elixir']['version'] = "1.3.4"
 default['elixir']['erlang']['version'] = "18.3"
