37dc37953571c838b69a618e9624fda663055c32
Christopher Rigor <crigor@gmail.com>
2016-12-26 08:33:05 +0800

    Include ec2 only

--- cookbooks/ey-init/recipes/main.rb
+++ cookbooks/ey-init/recipes/main.rb
@@ -11,15 +11,16 @@
 require 'pp'
 Chef::Log.info(ENV.pretty_inspect)
 
-include_recipe 'ey-base::chef_patches'
-include_recipe 'ey-base::resin_gems'
-include_recipe 'ey-core'
+#include_recipe 'ey-base::chef_patches'
+#include_recipe 'ey-base::resin_gems'
+#include_recipe 'ey-core'
+include_recipe "ec2" if ['solo', 'app', 'util', 'app_master','node'].include?(node.dna['instance_role'])
 
-include_recipe 'ey-custom::before-main'
+#include_recipe 'ey-custom::before-main'
 
-include_recipe 'ey-base::bootstrap' # common things that are installed in all the instances
-node.engineyard.instance.roles.each { |role| include_recipe "#{role}::prep" }
-node.engineyard.instance.roles.each { |role| include_recipe "#{role}::build" }
-include_recipe 'ey-base::post_bootstrap' # common things that we want to install setting up the instance
+#include_recipe 'ey-base::bootstrap' # common things that are installed in all the instances
+#node.engineyard.instance.roles.each { |role| include_recipe "#{role}::prep" }
+#node.engineyard.instance.roles.each { |role| include_recipe "#{role}::build" }
+#include_recipe 'ey-base::post_bootstrap' # common things that we want to install setting up the instance
 
-include_recipe 'ey-custom::after-main'
+#include_recipe 'ey-custom::after-main'
