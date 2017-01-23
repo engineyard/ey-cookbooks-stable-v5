6a785dc3142751e2a2787df5e3e8dcb8ab42c0a2
rbankston <rbankston@engineyard.com>
2017-01-23 18:00:31 -0500

    Updated Configuration

--- cookbooks/elixir/recipes/configure.rb
+++ cookbooks/elixir/recipes/configure.rb
@@ -1,15 +1,17 @@
 #create the vm.args File
 
+config = "/home/#{ssh_username}/your_app.config"
+name = `hostname`.chomp + "@" + node['ipaddress']
 
 managed_template "/home/#{ssh.username}/vm.args" do
   owner ssh_username
   group ssh_username
   mode 0644
-  source "haproxy.cfg.erb"
+  source "vmargs.erb"
   variables({
-    :name => sg@ec2-1.2.3.4.compute.amazonaws.com,
+    :name => name,
     :cookie => node.engineyard.environment.app_name,
-    :config => /home/ubuntu/your_app.config
+    :config => config
   })
 
 managed_template "/home/#{ssh.username}/node['elixir']['config']['file']" do
