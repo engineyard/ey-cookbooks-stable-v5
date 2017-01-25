40b5f266e2a6368e4ff988cdca0480a393846aaa
rbankston <rbankston@engineyard.com>
2017-01-25 18:48:34 -0500

    Fixing config files and syntax

--- cookbooks/elixir/recipes/configure.rb
+++ cookbooks/elixir/recipes/configure.rb
@@ -1,25 +1,28 @@
 #create the vm.args File
-
+ssh_username  = node['owner_name']
 config = "/home/#{ssh_username}/your_app.config"
 name = `hostname`.chomp + "@" + node['ipaddress']
 
-managed_template "/home/#{ssh.username}/vm.args" do
+node.engineyard.apps.each do |app|
+managed_template "/home/#{node["owner_name"]}/vm.args" do
   owner ssh_username
   group ssh_username
   mode 0644
   source "vmargs.erb"
   variables({
     :name => name,
-    :cookie => node.engineyard.environment.app_name,
+    :cookie => node.engineyard.environment['apps'],
     :config => config
   })
+end
 
-managed_template "/home/#{ssh.username}/node['elixir']['config']['file']" do
+managed_template "/home/#{node["owner_name"]}/node['elixir']['config']['file']" do
   owner ssh_username
   group ssh_username
   mode 0644
   source "elixir_app.config.erb"
   variables({
-    :optional_nodes => node.engineyard.environment.app_name,
+    :optional_nodes => node.engineyard.environment['apps'],
     :sync_timeout => 3000
   })
+end
