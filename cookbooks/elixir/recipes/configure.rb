4d7a2e6e752e9248b5406984765d084472934557
rbankston <rbankston@engineyard.com>
2017-01-25 19:31:08 -0500

    Prod.secret.exs

--- cookbooks/elixir/recipes/configure.rb
+++ cookbooks/elixir/recipes/configure.rb
@@ -26,3 +26,24 @@ managed_template "/home/#{node["owner_name"]}/node['elixir']['config']['file']" 
     :sync_timeout => 3000
   })
 end
+
+
+node.engineyard.apps.each do |app| do
+  managed_template "/data/#{app.name}/shared/config/database.yml" do
+  owner node.engineyard.environment.ssh_username
+  group node.engineyard.environment.ssh_username
+  mode 0600
+  source "database.yml.erb"
+  variables({
+    :determine_adapter_code => determine_adapter_code,
+    :environment => node.engineyard.environment['framework_env'],
+    :dbuser => node.engineyard.environment.ssh_username,
+    :dbpass => node.engineyard.environment.ssh_password,
+    :dbname => app.database_name,
+    :dbhost => node.dna['db_host'],
+    :dbtype => dbtype,
+    :slaves => node.engineyard.environment.instances.select{|i| i["role"] =="db_slave"},
+    :pool => node.engineyard.environment.jruby? ? node.dna['jruby_pool_size'] : nil
+  })
+end
+end
