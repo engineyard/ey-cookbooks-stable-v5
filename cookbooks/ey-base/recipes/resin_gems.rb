69434491aef5de4ac59729fc3379c7d7ed6681e2
Tyler Poland <tyler.poland@gmail.com>
2017-02-03 16:11:42 -0500

    adds eyrestore and updates resin

--- cookbooks/ey-base/recipes/resin_gems.rb
+++ cookbooks/ey-base/recipes/resin_gems.rb
@@ -2,7 +2,7 @@ resin_path = "/usr/local/ey_resin/ruby/bin"
 gem_bin_path = "/usr/local/ey_resin/bin"
 bin_path = '/usr/local/bin'
 
-["eybackup", "ey-snapshots", "ey-snaplock"].each do |executable|
+["eybackup", "eyrestore", "ey-snapshots", "ey-snaplock"].each do |executable|
   link "#{bin_path}/#{executable}" do
     to "#{gem_bin_path}/#{executable}"
     only_if { File.exists?("#{gem_bin_path}/#{executable}") }
