95f355c02c5ffe66321ca15fb189c24a4d9c5675
Mikong Galero <mgalero@engineyard.com>
2017-02-21 18:04:25 +0800

    pm2 recipe: install, link, init setup, app binary.

---
+++ cookbooks/pm2/metadata.rb
@@ -0,0 +1,3 @@
+name 'pm2'
+maintainer 'Engine Yard'
+maintainer_email 'support@engineyard.com'
---
+++ cookbooks/pm2/recipes/default.rb
@@ -0,0 +1,34 @@
+execute "install pm2" do
+  command "npm install -g pm2"
+  notifies :create, "link[/usr/bin/pm2]"
+end
+
+link "/usr/bin/pm2" do
+  to "/opt/nodejs/current/bin/pm2"
+  # only_if { File.exist?("/opt/nodejs/current/bin/pm2") }
+  # not_if { File.exists?("/usr/bin/pm2") }
+  notifies :run, "execute[start pm2 at boot]"
+  action :nothing
+end
+
+execute "start pm2 at boot" do
+  command "pm2 startup openrc"
+  # only_if { ::File.exists?("/usr/bin/pm2") }
+  action :nothing
+end
+
+node.engineyard.apps.each_with_index do |app, app_index|
+
+  app_name = app.name
+
+  template "/engineyard/bin/app_#{app_name}" do
+    source "app_control.sh.erb"
+    owner node["owner_name"]
+    group node["owner_name"]
+    backup 0
+    mode 0755
+    variables(
+      :app_name => app_name
+    )
+  end
+end
