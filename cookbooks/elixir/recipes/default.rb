9991581c30556b31c21d2ef5d9ffe3c38790ff46
rbankston <rbankston@engineyard.com>
2017-03-07 13:28:10 -0500

    Changing order of operations

--- cookbooks/elixir/recipes/default.rb
+++ cookbooks/elixir/recipes/default.rb
@@ -1,5 +1,5 @@
-include_recipe "elixir::install"
-include_recipe "elixir::configure"
 if ['app_master', 'app', 'solo'].include? node.dna['instance_role']
   include_recipe "elixir::nginx"
 end
+include_recipe "elixir::install"
+include_recipe "elixir::configure"
