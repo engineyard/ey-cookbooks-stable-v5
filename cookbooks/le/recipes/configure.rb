13102f009a245308044b39c403cbc9bb7b5a70ba
Radamanthus Batnag <radamanthus@gmail.com>
2017-03-10 09:36:18 +0800

    Ensure unique agent-key for each instance

--- cookbooks/le/recipes/configure.rb
+++ cookbooks/le/recipes/configure.rb
@@ -4,7 +4,7 @@
 #
 
 execute "le register --account-key" do
-  command "/usr/bin/le register --account-key #{node['le']['le_api_key']} --name #{node['dna']['applications'].keys.first}"
+  command "/usr/bin/le register --account-key #{node['le']['le_api_key']} --name #{node['dna']['applications'].keys.first}/#{node['dna']['engineyard']['this']}"
   action :run
   not_if { File.exists?('/etc/le/config') }
 end
