a4a010233f714cd34ae135072931010fc3147dc2
rbankston <rbankston@engineyard.com>
2017-03-14 11:44:54 -0400

    Fixed recipe for util instances

--- cookbooks/elixir/recipes/configure.rb
+++ cookbooks/elixir/recipes/configure.rb
@@ -54,6 +54,13 @@ end
 
 node.engineyard.apps.each_with_index do |app, index|
 
+  nginx_http_port = 8081
+  nginx_https_port = 8082
+  base_port = node['elixir']['port'].to_i
+  stepping = 200
+  app_base_port = base_port + ( stepping * index )
+
+
   managed_template "/data/#{app.name}/shared/config/prod.secret.exs" do
     owner node.engineyard.environment.ssh_username
     group node.engineyard.environment.ssh_username
@@ -66,7 +73,7 @@ node.engineyard.apps.each_with_index do |app, index|
       :dbname => app.database_name,
       :app_name => app.name,
       :db_host => node.dna['db_host'] ,
-      :port => port
+      :port => app_base_port
     })
   end
 
@@ -96,27 +103,6 @@ node.engineyard.apps.each_with_index do |app, index|
       :app => app.name,
       :user => node["owner_name"]
     )
-  end
-
-    nginx_http_port = 8081
-    nginx_https_port = 8082
-    base_port = node['elixir']['port'].to_i
-    stepping = 200
-    app_base_port = base_port + ( stepping * index )
-
-  template "/data/nginx/servers/#{app.name}.conf" do
-    owner ssh_username
-    group ssh_username
-    mode 0644
-    source "nginx_app.conf.erb"
-    cookbook "elixir"
-    variables({
-      :vhost => app.vhosts.first,
-      :port => nginx_http_port,
-      :upstream_port => port,
-      :framework_env => framework_env
-    })
-    notifies :restart, resources(:service => "nginx"), :delayed
   end
 
   template "/engineyard/bin/app_#{app.name}" do
