d176f7365584957e4ddd711f6bbd95b52c795ebc
Tyler Poland <tyler.poland@gmail.com>
2017-03-17 13:12:19 -0400

    Add support for pending environment timezone feature.

--- cookbooks/timezones/README.md
+++ cookbooks/timezones/README.md
@@ -1,4 +1,4 @@
 timezones
 ========
 
-Updates the timezone-data package. Attributes/default.rb maintains the version number.
+Updates the timezone-data package. Attributes/default.rb maintains the version number. Sets timezone based on timezone information in DNA if it exists.
--- cookbooks/timezones/recipes/default.rb
+++ cookbooks/timezones/recipes/default.rb
@@ -7,3 +7,25 @@ package "sys-libs/timezone-data" do
   version node['timezones']['version']
   action :upgrade
 end
+
+zonepath = '/usr/share/zoneinfo/'
+zone = "#{node[:engineyard][:environment][:timezone]}"
+
+has_nginx = ['solo','app','app_master'].include?(node['instance_role'])
+
+if not File.exists?(File.join(zonepath, zone)) and zone != '' and not zone.nil?
+  raise "Timezone '#{zone}' not recognized."
+end
+
+service "vixie-cron"
+service "sysklogd"
+service "nginx"
+
+link '/etc/localtime' do
+  to "#{File.join(zonepath, zone)}"
+  notifies :restart, resources(:service => ["vixie-cron", "sysklogd"]), :delayed
+  if has_nginx
+    notifies :restart, 'service[nginx]', :delayed
+  end
+  only_if {File.exists?(File.join(zonepath, zone)) and zone != '' and not zone.nil?}
+end
