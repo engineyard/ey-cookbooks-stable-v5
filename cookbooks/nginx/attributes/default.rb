e9b09834719f7af8a16d640f468d3e9b9dd26bc8
Dimitris Dalianis <ddalianis@engineyard.com>
2017-10-11 12:12:59 +0300

    hello HTTP/2 world

--- cookbooks/nginx/attributes/default.rb
+++ cookbooks/nginx/attributes/default.rb
@@ -2,6 +2,7 @@ action = node.engineyard.metadata(:nginx_action,:restart)
 using_openssl_101 = (node.engineyard.metadata('openssl_ebuild_version','1.0.1') =~ /1\.0\.1/)
 default['nginx']['version'] = node.engineyard.metadata('nginx_ebuild_version','1.8.1-r1')
 default['nginx']['action'] = action
+default['nginx']['http2'] = false
 
 
 Chef::Log.info("Version: #{nginx[:version]}, Passenger Gem:#{nginx[:passenger_gem]}\n")
