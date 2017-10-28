6d129ef826810a6e33a69415a8913fac58fb96e3
Vijay Raghavan Aravamudhan <avijayr - at - gmail - dot - com>
2017-10-28 14:44:06 +0530

    #282 Make puma restart sleep timeout configurable.

--- cookbooks/puma/attributes/puma.rb
+++ cookbooks/puma/attributes/puma.rb
@@ -1,1 +1,7 @@
-default['puma']['workers'] = node['cpu']['total']
+default['puma'].tap do |puma|
+  # Number of workers (not threads)
+  puma['workers'] = node['cpu']['total']
+
+  # sleep timeout
+  puma['sleep_timeout'] = 4
+end
