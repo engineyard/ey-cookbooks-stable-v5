c3419346dfa86a45690d5393609e5b17d5f9720c
Radamanthus Batnag <radamanthus@gmail.com>
2017-11-07 08:31:27 +0800

    Use Nginx 1.12.1 by default (#289)

--- cookbooks/nginx/attributes/default.rb
+++ cookbooks/nginx/attributes/default.rb
@@ -1,6 +1,6 @@
 action = node.engineyard.metadata(:nginx_action,:restart)
 using_openssl_101 = (node.engineyard.metadata('openssl_ebuild_version','1.0.1') =~ /1\.0\.1/)
-default['nginx']['version'] = node.engineyard.metadata('nginx_ebuild_version','1.8.1-r1')
+default['nginx']['version'] = node.engineyard.metadata('nginx_ebuild_version','1.12.1')
 default['nginx']['action'] = action
 default['nginx']['http2'] = false
 
