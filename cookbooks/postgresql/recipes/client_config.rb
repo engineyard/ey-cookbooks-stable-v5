a85df2269195201caf68d1d197caf7d4aab8e93c
Tyler Poland <tyler.poland@gmail.com>
2017-11-22 17:32:04 -0500

    fixup to remove PGHOST entry from .bash_login; this can block replica promotions

--- cookbooks/postgresql/recipes/client_config.rb
+++ cookbooks/postgresql/recipes/client_config.rb
@@ -15,8 +15,15 @@ end
 
 update_file "add_PGUSER_to_root_bash_login" do
   path "/root/.bash_login"
-  body "export PGUSER='#{node.engineyard.environment['db_admin_username']}'\nexport PGHOST='#{node.dna['db_host']}'\nexport PGDATABASE=postgres"
+  body "export PGUSER='#{node.engineyard.environment['db_admin_username']}'\nexport PGDATABASE=postgres"
   not_if "grep 'PGDATABASE' /root/.bash_login"
+end
+
+# cleanup PGHOST entries if set, this was a bug introduced by stable-v5-3.0.33 (October 2017)
+  # these wouldn't have updated on a replica promotion or a db master hostname/ip change
+  # better way to approach these would be to write a .ey_login and then source it from .bash_login (to be addressed in v6)
+execute "remove PGHOST entries if present" do
+  command "sed -i '/^export PGHOST=.*/d' /root/.bash_login"
 end
 
 cookbook_file "/root/.psqlrc" do
