45be5dea9907264477e71f8c5c058eb9447c2178
root <jevgenij@lmiw.net>
2017-12-04 13:38:28 +0000

    default openssl version update for the openssl cookbook. Ticket: CC-1146

--- cookbooks/openssl/attributes/openssl.rb
+++ cookbooks/openssl/attributes/openssl.rb
@@ -1,2 +1,3 @@
 default.openssl.using_metadata = !!node.engineyard.metadata("openssl_ebuild_version",nil)
-default.openssl.version = node.engineyard.metadata('openssl_ebuild_version','1.0.2j')
+# YT-CC-1146
+default.openssl.version = node.engineyard.metadata('openssl_ebuild_version','1.0.2k')
