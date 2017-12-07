297770a3251984578d386f008e49ad8aeeb5d1fc
Daniel Beckstead <dbeckstead@engineyard.com>
2017-12-06 19:43:42 -0800

    Revert "dev-lang/php package was updated to the version 5.6.30. Ticket: 1119"
    This reverts commit 5cdecc106e314b2e74b3a40d46afdd31f94ba714.

--- cookbooks/php/attributes/php.rb
+++ cookbooks/php/attributes/php.rb
@@ -2,13 +2,13 @@ default['php']['full_atom'] = "dev-lang/php"
 
 default['php']['version'] = case attribute['dna']['engineyard']['environment']['components'].map(&:values).flatten.find(/^php_/).first
   when 'php_56'
-    '5.6.30'
+    '5.6.25'
   when 'php_7'
     '7.0.11'
   when 'php_71'
       '7.1.0'
   else
-   '5.6.30'
+   '5.6.25'	
 end
  
 default['php']['minor_version'] =  default['php']['version'].split(".").first(2).join(".")
