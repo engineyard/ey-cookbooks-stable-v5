65e6c6a8d9b2fae7970d9533683380f779e50b90
Daniel Beckstead <dbeckstead@engineyard.com>
2017-12-06 19:53:26 -0800

    Revert "default openssl version update for the openssl cookbook. Ticket: CC-1146"
    This reverts commit 45be5dea9907264477e71f8c5c058eb9447c2178.

--- cookbooks/openssl/attributes/openssl.rb
+++ cookbooks/openssl/attributes/openssl.rb
@@ -1,3 +1,2 @@
 default.openssl.using_metadata = !!node.engineyard.metadata("openssl_ebuild_version",nil)
-# YT-CC-1146
-default.openssl.version = node.engineyard.metadata('openssl_ebuild_version','1.0.2k')
+default.openssl.version = node.engineyard.metadata('openssl_ebuild_version','1.0.2j')
