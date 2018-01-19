a0bed6c97c7eed7db1bdb1e792559ca8e189e20d
Radamanthus Batnag <radamanthus@gmail.com>
2018-01-19 16:31:58 +0800

    Adds the Redis Sentinel recipe

---
+++ cookbooks/redis-sentinel/metadata.rb
@@ -0,0 +1,10 @@
+name 'redis-sentinel'
+description 'Configuration & deployment of Redis Sentinel on Engine Yard'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+maintainer 'Engine Yard'
+maintainer_email 'support@engineyard.com'
+version '5.0.0'
+source_url 'https://github.com/engineyard/ey-cookbooks-stable-v5'
+issues_url 'https://github.com/engineyard/ey-cookbooks-stable-v5/issues'
+
+depends 'redis'
---
+++ custom-cookbooks/redis-sentinel/README.md
@@ -0,0 +1,6 @@
+# Redis Sentinel
+
+This example contains a complete cookbooks/ that you can use on the stable-v5 stack.
+
+See https://github.com/engineyard/ey-cookbooks-stable-v5/tree/next-release/custom-cookbooks/redis-sentinel/cookbooks/custom-redis-sentinel for complete instructions.
+
