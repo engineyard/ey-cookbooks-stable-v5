68caffb63c530863eb66900cfcbab00c0c012d6c
Gordon Malm <gmalm+git-v5cookbooks-001@engineyard.com>
2018-04-19 12:42:30 -0700

    Fix load_current_resource to correctly read version strings ending in a lower-case alpha character. YT-CC-1244.

--- cookbooks/ey-base/recipes/chef_patches.rb
+++ cookbooks/ey-base/recipes/chef_patches.rb
@@ -1,17 +1,53 @@
 #
 # Changes for Chef 12.7.2 and 12.10.24:
 # YT-CC-1236: Rewrite candidate_version determination for increased speed and reduced complexity/error-chance.
+# YT-CC-1244: Fix load_current_resource to correctly read version strings ending in a lower-case alpha character.
 #
 
 require 'chef/provider/package/portage'
 
 ChefPatches = {
-  '12.7.2' => [:candidate_version, :install_package],
-  '12.10.24' => [:candidate_version, :install_package]
+  '12.7.2' => [:candidate_version, :install_package, :load_current_resource],
+  '12.10.24' => [:candidate_version, :install_package, :load_current_resource]
 }
 
 unless ChefPatches.has_key?(Chef::VERSION)
   raise Chef::Exceptions::Package, "This version of Chef (#{Chef::VERSION}) may not correctly handle explicit portage categories -- see cookbooks/ey-base/recipes/chef_patches.rb"
+end
+
+if ChefPatches[Chef::VERSION].include? :load_current_resource
+  class Chef::Provider::Package::Portage
+
+    def load_current_resource
+      @current_resource = Chef::Resource::Package.new(@new_resource.name)
+      @current_resource.package_name(@new_resource.package_name)
+
+      category, pkg = %r{^#{PACKAGE_NAME_PATTERN}$}.match(@new_resource.package_name)[1, 2]
+
+      globsafe_category = category ? Chef::Util::PathHelper.escape_glob_dir(category) : nil
+      globsafe_pkg = Chef::Util::PathHelper.escape_glob_dir(pkg)
+      possibilities = Dir["/var/db/pkg/#{globsafe_category || "*"}/#{globsafe_pkg}-*"].map { |d| d.sub(%r{/var/db/pkg/}, "") }
+      versions = possibilities.map do |entry|
+	if entry =~ %r{[^/]+/#{Regexp.escape(pkg)}\-(\d[\.\d]*[a-z]?((_(alpha|beta|pre|rc|p)\d*)*)?(-r\d+)?)}
+          [$&, $1]
+        end
+      end.compact
+
+      if versions.size > 1
+        atoms = versions.map { |v| v.first }.sort
+        categories = atoms.map { |v| v.split("/")[0] }.uniq
+        if !category && categories.size > 1
+          raise Chef::Exceptions::Package, "Multiple packages found for #{@new_resource.package_name}: #{atoms.join(" ")}. Specify a category."
+        end
+      elsif versions.size == 1
+        @current_resource.version(versions.first.last)
+        Chef::Log.debug("#{@new_resource} current version #{$1}")
+      end
+
+      @current_resource
+    end
+
+  end
 end
 
 if ChefPatches[Chef::VERSION].include? :candidate_version
