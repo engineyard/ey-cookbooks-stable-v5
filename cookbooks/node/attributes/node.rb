1759ce2fe82c95345078bd75657d27fcc1c1d02c
Johann Fuechsl <johann@fuechsl.co>
2018-09-18 13:29:09 +0200

    Enable support for node.js series 8.x, 9.x, and 10.x

--- cookbooks/node/attributes/node.rb
+++ cookbooks/node/attributes/node.rb
@@ -14,6 +14,12 @@ fuzzy_version = begin
                 end
 
 fallback_nodejs_version = case (fuzzy_version || hard_version)
+                           when 'nodejs_10'
+                             '10.10.0'
+                           when 'nodejs_9'
+                             '9.11.2'
+                           when 'nodejs_8'
+                             '8.12.0'
                            when 'nodejs_6'
                              '6.9.5'
                            when 'nodejs_5'
@@ -31,7 +37,10 @@ default['nodejs']['available_versions'] = [
   '5.11.0', # net-libs/nodejs-5.11.0
   '6.4.0', # net-libs/nodejs-6.4.0
   '6.7.0', # net-libs/nodejs-6.7.0
-  '6.9.5' # net-libs/nodejs-6.9.5
+  '6.9.5', # net-libs/nodejs-6.9.5
+  '8.12.0', # net-libs/nodejs-bin-8.12.0
+  '9.11.2', # net-libs/nodejs-bin-9.11.2
+  '10.10.0' # net-libs/nodejs-bin-10.10.0
 ]
 
 if (node.engineyard.metadata('openssl_ebuild_version','1.0.1') =~ /1\.0\.1/)
@@ -42,8 +51,10 @@ if (node.engineyard.metadata('openssl_ebuild_version','1.0.1') =~ /1\.0\.1/)
     '5.11.0', # net-libs/nodejs-5.11.0
     '6.4.0', # net-libs/nodejs-6.4.0
     '6.7.0', # net-libs/nodejs-6.7.0
-    '6.9.5' # net-libs/nodejs-6.9.5
-
+    '6.9.5', # net-libs/nodejs-6.9.5
+    '8.12.0', # net-libs/nodejs-bin-8.12.0
+    '9.11.2', # net-libs/nodejs-bin-9.11.2
+    '10.10.0' # net-libs/nodejs-bin-10.10.0
   ])
 end
 
