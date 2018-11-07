986f8d7f14ebd4bc5d2a7599df2d142d1c257455
Johann Fuechsl <johann@fuechsl.co>
2018-11-07 08:28:44 +0100

    Wait for swap partition to exist before running mkswap

--- cookbooks/ey-core/recipes/swap.rb
+++ cookbooks/ey-core/recipes/swap.rb
@@ -2,6 +2,9 @@ bash "make-swap-xvdc" do
   code <<-EOH
     parted -s -a optimal /dev/xvdc mklabel msdos
     parted -s -a optimal -- /dev/xvdc unit compact mkpart primary linux-swap "1" "-1"
+    while [[ ! -b /dev/xvdc1 ]]; do
+      sleep 1
+    done
     mkswap /dev/xvdc1
     swapon /dev/xvdc1
     echo "/dev/xvdc1 swap swap sw 0 0" >> /etc/fstab
