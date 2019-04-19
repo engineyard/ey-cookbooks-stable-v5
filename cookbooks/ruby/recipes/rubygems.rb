dfb0b248ff26bb0b976aacf51aeecbc5fadf6019
EY Chef <chef@engineyard.com>
2019-04-19 08:47:07 +0800

    Remove bundler installed by rubygems

--- cookbooks/ruby/recipes/rubygems.rb
+++ cookbooks/ruby/recipes/rubygems.rb
@@ -79,4 +79,10 @@ if node.engineyard.environment.ruby?
   else
     ensure_rubygems_version
   end
+
+  # removing packaged bundler prevents the error "You must use Bundler 2 or greater with this lockfile"
+  # engineyard-serverside installs bundler during deploys
+  execute "remove bundler installed by rubygems" do
+    command "rm -rf /usr/lib64/ruby/site_ruby/*/bundler{,.rb} && rm -f /usr/local/lib64/ruby/gems/*/specifications/default/bundler*gemspec"
+  end
 end
