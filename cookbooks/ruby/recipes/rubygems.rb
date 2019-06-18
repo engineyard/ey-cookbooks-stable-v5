2b4ecb7523209f8f38715cdc777d0dc46fccbc74
Johann Fuechsl <johann@fuechsl.co>
2019-06-18 11:46:43 +0200

    Support for Ruby 2.6

--- cookbooks/ruby/recipes/rubygems.rb
+++ cookbooks/ruby/recipes/rubygems.rb
@@ -3,6 +3,10 @@ if node.engineyard.environment.ruby?
 
   # removing packaged bundler prevents the error "You must use Bundler 2 or greater with this lockfile"
   # engineyard-serverside installs bundler during deploys
+  package "remove bundler OS package" do
+    package_name "dev-ruby/bundler"
+    action :remove
+  end
   execute "remove bundler installed by rubygems" do
     command "rm -rf /usr/lib64/ruby/site_ruby/*/bundler{,.rb} && rm -f /usr/local/lib64/ruby/gems/*/specifications/default/bundler*gemspec"
   end
