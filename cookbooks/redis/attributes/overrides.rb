531320b2e3f5ba6eb2ad99fd7df430e7c1dcad3d
Pasan Chamikara <gwpc92@gmail.com>
2019-08-30 22:37:14 -0700

    [FB-851] Redis Version Changed to 3.2.0-rc3. Install from Source enabled to True

--- cookbooks/redis/attributes/overrides.rb
+++
@@ -1,3 +0,0 @@
-# Overrides default Redis version with the latest available 
-# FB-851
-node.set['redis']['version'] = "3.2.0-r2"
--- custom-cookbooks/redis/cookbooks/custom-redis/attributes/overrides.rb
+++
@@ -1,2 +0,0 @@
-# FB-851
-node.set['redis']['version'] = "3.2.0-r2"
