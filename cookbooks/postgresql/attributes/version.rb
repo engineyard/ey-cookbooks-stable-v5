6e375d7cdc537f984454dd24188bc5fdd40f3c2f
Pasan Chamikara <gwpc92@gmail.com>
2019-11-27 11:09:31 +0530

    [FB-854] PostgreSQL versions updated to latest available versions. v10=>10.10, v9.6=>9.6.15, 9.5=> 9.5.15, 9.4=>9.4.24

--- cookbooks/postgresql/attributes/version.rb
+++ cookbooks/postgresql/attributes/version.rb
@@ -1,15 +1,15 @@
 case attribute.dna.engineyard.environment.db_stack_name
 when "postgres9_4"
-  default['postgresql']['latest_version'] = '9.4.22'
+  default['postgresql']['latest_version'] = '9.4.24'
   default['postgresql']['short_version'] = '9.4'
 when "postgres9_5"
-  default['postgresql']['latest_version'] = '9.5.17'
+  default['postgresql']['latest_version'] = '9.5.19'
   default['postgresql']['short_version'] = '9.5'
 when "postgres9_6"
-  default['postgresql']['latest_version'] = '9.6.13'
+  default['postgresql']['latest_version'] = '9.6.15'
   default['postgresql']['short_version'] = '9.6'
 when "postgres10"
-  default['postgresql']['latest_version'] = '10.8'
+  default['postgresql']['latest_version'] = '10.10'
   default['postgresql']['short_version'] = '10'
 end
 default['postgresql']['datadir'] = "/db/postgresql/#{node['postgresql']['short_version']}/data/"
