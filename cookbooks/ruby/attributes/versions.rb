dd5c4a0710f493ea6b148ba5388b797041fe5edb
Callum Pease <cpease1997@hotmail.com>
2020-05-15 00:32:54 +0530

    Update versions.rb

--- cookbooks/ruby/attributes/versions.rb
+++ cookbooks/ruby/attributes/versions.rb
@@ -38,7 +38,7 @@ elsif ruby2x?(3, ruby_version)
   }
 elsif ruby2x?(4, ruby_version)
   default[:ruby_dependencies] = {
-    "dev-ruby/json"         => "2.0.3",
+    "dev-ruby/json"         => "2.3.0",
     "dev-ruby/racc"         => "1.4.14-r1",
     "dev-ruby/rake"         => "12.0.0",
     "dev-ruby/rdoc"         => "5.1.0",
@@ -74,7 +74,7 @@ elsif ruby2x?(5, ruby_version)
 elsif ruby2x?(6, ruby_version)
   default[:ruby_dependencies] = {
     "dev-ruby/bundler"      => "1.17.3",
-    "dev-ruby/json"         => "2.0.3-r2",
+    "dev-ruby/json"         => "2.3.0",
     "dev-ruby/racc"         => "1.4.14-r3",
     "dev-ruby/rake"         => "12.3.3",
     "dev-ruby/rdoc"         => "6.0.3-r1",
